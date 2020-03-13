3f28e45dc2a793625a82e965ff8904586d6abd63
Alejandro <orochi_alex@hotmail.com>
2020-03-12 23:33:57 -0600

    added loger

--- src/main/groovy/com/mx/finerio/services/imp/HtmlHandlerServiceImp.groovy
+++ src/main/groovy/com/mx/finerio/services/imp/HtmlHandlerServiceImp.groovy
@@ -3,12 +3,14 @@ package com.mx.finerio.services.imp
 import com.mx.finerio.dtos.Podcast
 import com.mx.finerio.services.HtmlHandlerService
 import com.mx.finerio.services.JsoupService
+import org.apache.log4j.Logger
 import org.jsoup.HttpStatusException
 
 class HtmlHandlerServiceImp implements HtmlHandlerService {
 
-    public static final String BBC_PODCAST_URL = 'https://www.bbc.co.uk/programmes/p086934c/episodes/downloads'
+    public static final String BBC_PODCAST_URL = 'https://www.bbc.co.uk/programmes/p086934c/episodes/downloadsX'
     public static final String CLASS = 'link-complex popup__list__item island--squashed br-subtle-bg-ontext br-subtle-bg-onbg--hover br-subtle-link-ontext--hover'
+    private static Logger log = Logger.getLogger(HtmlHandlerServiceImp.class);
 
     JsoupService jsoupService = new JsoupServiceImp()
 
@@ -19,7 +21,7 @@ class HtmlHandlerServiceImp implements HtmlHandlerService {
             podcast = jsoupService.getHtmlPage(BBC_PODCAST_URL)
                     .getElementsByClass(CLASS).collect({ it -> new Podcast(it) })
         }catch(HttpStatusException e){
-            println 'Can not download podcast: ' << e.message //TODO place it into logger
+            log.info( 'Can not download podcast: ' << e.message)
         }
         podcast
     }
