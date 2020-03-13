de3c84db090431a4be95e049fd4d56136750e6b0
Alejandro <orochi_alex@hotmail.com>
2020-03-13 14:27:24 -0600

    added quality filter

--- src/main/groovy/com/mx/finerio/controller/imp/DownloadControllerImp.groovy
+++ src/main/groovy/com/mx/finerio/controller/imp/DownloadControllerImp.groovy
@@ -3,18 +3,21 @@ package com.mx.finerio.controller.imp
 import com.mx.finerio.controller.DownloadController
 import com.mx.finerio.dtos.Arguments
 import com.mx.finerio.dtos.Podcast
+import com.mx.finerio.enums.Quality
 import com.mx.finerio.services.DownloadService
 import com.mx.finerio.services.HtmlHandlerService
 import com.mx.finerio.services.imp.DownloadServiceImp
 import com.mx.finerio.services.imp.HtmlHandlerServiceImp
 import org.apache.log4j.Logger
 
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
 class DownloadControllerImp implements DownloadController {
 
     public static final String DEFAULT_CATEGORY = 'p086934c/episodes/downloads'
 
     private static String BBC_PODCAST_URL(String category) {"https://www.bbc.co.uk/programmes/${category}"}
-
 
     private static Logger log = Logger.getLogger(DownloadControllerImp.class)
 
@@ -23,20 +26,29 @@ class DownloadControllerImp implements DownloadController {
 
     @Override
     void downloadPodcasts(String[] args){
-
         Arguments arguments = buildArguments(args)
-
-        def bbc_podcast_url = BBC_PODCAST_URL(arguments.categoryToken ? arguments.categoryToken : DEFAULT_CATEGORY)
         List<Podcast> podcasts = handlerService.podcastFromHtml(
-                bbc_podcast_url
+                BBC_PODCAST_URL(arguments.categoryToken ? arguments.categoryToken : DEFAULT_CATEGORY)
         )
-
         if(podcasts.isEmpty()){
             log.error("No podcast found on page")
             return
         }
+        isQualityArgsFilterDownload(arguments) ? filteredDownload(podcasts, arguments) : fullQualityDownload(podcasts)
+    }
 
-        podcasts.stream().parallel().each {downloadService.tryDownload(it)}
+    private Stream<Podcast> fullQualityDownload(List<Podcast> podcasts) {
+        podcasts.stream().parallel().each { downloadService.tryDownload(it) }
+    }
+
+    private static boolean isQualityArgsFilterDownload(Arguments arguments) {
+        arguments.quality == Quality.HIGHER.value || arguments.quality == Quality.LOWER.value
+    }
+
+    private Stream<Podcast> filteredDownload(List<Podcast> podcasts, arguments) {
+        podcasts.stream()
+                .filter({ podcast -> podcast.quality == arguments.quality })
+                .parallel().each { downloadService.tryDownload(it) }
     }
 
     private static Arguments buildArguments(String[] args) {
